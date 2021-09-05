59dbab55b2c65e86c93b5b3d2667c04976d64229
chamikara <chamikara98@gmail.com>
2021-09-06 02:20:41 +0530

    added shopingcart icon to the button

--- src/pages/home/bookTile.jsx
+++ src/pages/home/bookTile.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "./styles/bookTile.css";
 import { Link } from "react-router-dom";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class BookTile extends Component {
   state = {};
   render() {
     return (
       <div className="container bookTile" style={{ marginBottom: "10px" }}>
-        <h6 style={{ fontSize: "0.82rem" }}>{this.props.name}</h6>
+        <h6 style={{ fontSize: "1rem" }}>{this.props.name}</h6>
         <div className="row image">
           <Link to={`/books/${this.props.id}`}>
             <img src={this.props.imageUrl} alt="" />
@@ -15,12 +17,12 @@ class BookTile extends Component {
         </div>
         {/* <div className="row priceRow"> */}
         <div>
-          <h6 style={{ fontSize: "0.8rem" }}>Price: Rs.{this.props.price} </h6>
+          <h6 style={{ fontSize: "0.9rem" }}>Price: Rs.{this.props.price} </h6>
           <button
-            className="btn btn-primary btn-sm"
-            style={{ fontSize: "0.6rem" }}
+            className="btn btn-outline-secondary btn-md"
+            style={{ fontSize: "0.69rem" }}
           >
-            Add to Cart 
+            <FontAwesomeIcon icon={faShoppingCart} />  Add to Cart  
           </button>
         </div>
         {/* <div className="col-6"></div> */}

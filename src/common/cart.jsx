22e25a82b7a30e69b86c2a9ff14145ffd571edee
chamikara <chamikara98@gmail.com>
2021-09-20 23:01:52 +0530

    Added price to the cart

--- src/common/cart.jsx
+++ src/common/cart.jsx
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 import { useSelector } from "react-redux";
 
-
 const Cart = () => {
-
   const selectedBooks = useSelector((state) => state.cart.selectedBooks);
-
 
   return (
     <div>
-      <table class="table">
-        <thead style={{ color: "white" }}>
+      <table class="table" style={{ color: "white" }}>
+        <thead>
           <tr>
             <th scope="col">Name</th>
             <th scope="col">Author</th>
             <th scope="col">Qty</th>
+            <th scope="col">Price</th>
           </tr>
         </thead>
         <tbody>
           {selectedBooks.map((book) => (
-            <tr style={{ color: "white" }}>
+            <tr>
               <td>{book.name}</td>
               <td>{book.authors[0]}</td>
               <td>{book.selectedBookQty}</td>
+              <td>{`Rs. ${book.selectedBookQty * book.price}`}</td>
             </tr>
           ))}
         </tbody>

86bc43f8e2a889cbfe897fb0b3d2c59cd3e406c9
chamikara <chamikara98@gmail.com>
2021-09-23 01:39:26 +0530

    Code Formatting

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,13 +1,11 @@
 import { combineReducers } from "redux";
 import { appReducer } from "./appReducer";
-import { chamiReducer } from "./chamiReducer";
 import { bookReducer } from "./bookReducer";
 import { cartReducer } from "./cartReducer";
 import { userReducer } from "./userReducer";
 
 export const rootReducer = combineReducers({
   app: appReducer,
-  chami: chamiReducer,
   book: bookReducer,
   cart: cartReducer,
   user: userReducer,
